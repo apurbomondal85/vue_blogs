98f81eb2ff817fff3306228d9c7d8c9df4518895
Apurbo Mondal <apurbo.simt.2020@gmail.com>
2025-03-05 20:32:48 +0600

    header section is done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/header.vue'
+
+export default {
+  components: {
+    Header
+  },
+}
 </script>
 
 <template>
+  <Header />
   <RouterView />
 </template>
 
